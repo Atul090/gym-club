5d081b15c9c6832e0d398d124b67ae3ed496570d
Atul Deep Singh <999.atulsingh@gmail.com>
2024-12-27 19:37:16 +0530

    added css header

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Header from '../Header/Header'
 import './Hero.css'
 const Hero = () => {
   return (
     <div className='hero'>
         <div className='left-h'> Left side</div>
+        <Header />
         <div className='right-h'> Right side</div>
     </div>
   )
